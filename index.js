eba21e1672c459f9633f7bb8a73cfeb7386f21d2
DamirMurzomahamatov <you@example.com>
2022-03-18 22:18:21 +0500

    homework10

---
+++ index.js
@@ -0,0 +1,59 @@
+let discount = 20
+
+let arr = [
+    {
+        id: 1,
+        elem: 'Bread',
+        price: '5000'   
+    },
+    {
+        id: 2,
+        elem: 'pizza',
+        price: '50000'   
+    },
+    {
+        id: 3,
+        elem: 'water',
+        price: '10000'   
+    },
+    {
+        id: 4,
+        elem: 'meat',
+        price: '80000'   
+    },
+    {
+        id: 5,
+        elem: 'egs',
+        price: '35000'   
+    },
+    {
+        id: 6,
+        elem: 'rolton',
+        price: '5000'   
+    },
+    {
+        id: 7,
+        elem: 'maslo',
+        price: '18000'   
+    },
+    {
+        id: 8,
+        elem: 'ramyon',
+        price: '22000'   
+    },
+    {
+        id: 9,
+        elem: 'tvorog',
+        price: '7000'   
+    },
+    {
+        id: 10,
+        elem: 'vodka',
+        price: '10000'   
+    }
+]
+
+for(let item of arr) {
+ item.sale=item.price-item.price*discount/100
+}
+console.log(arr);
